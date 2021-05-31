75dd9b9a4178e0282ab70ee2644eeea7acbcc720
PapayaHUANG <hsy922@qq.com>
2021-05-31 19:45:19 +0800

    bug fixed

--- music_player.js
+++ music_player.js
@@ -1,107 +1,108 @@
 const previous = document.getElementById("previous");
 const play = document.getElementById("play");
 const next = document.getElementById("next");
-const music =document.getElementById("music");
+const music = document.getElementById("music");
 const img = document.getElementById("img");
-const title =document.getElementById("title");
+const title = document.getElementById("title");
 const controlPad = document.querySelector(".control-pad");
 const display = document.querySelector(".display");
 const progress = document.getElementById("progress");
+const progressContainer = document.getElementById("progress-container");
 
-
-
-const musicList = ["漩涡", "苦瓜", "罗生门","少女的祈祷","天下无双"];
+const musicList = ["漩涡", "苦瓜", "罗生门", "少女的祈祷", "天下无双"];
 let musicIndex = 0;
 
 loadMusic(musicList[musicIndex]);
 
 //load music
 function loadMusic() {
-    music.setAttribute('src', `/musics/${musicList[musicIndex]}.mp3`);
-    img.setAttribute('src', `/snapshots/${musicList[musicIndex]}.jpeg`);
-    title.innerText = `${musicList[musicIndex]}`;
+  music.setAttribute("src", `/musics/${musicList[musicIndex]}.mp3`);
+  img.setAttribute("src", `/snapshots/${musicList[musicIndex]}.jpeg`);
+  title.innerText = `${musicList[musicIndex]}`;
 }
-
-
 
 //play music
 
 function musicPlay() {
-    controlPad.classList.add('play');
-    img.style.animationPlayState = 'running';
-    display.style.transform = 'translateY(-85%)';
-    progress.style.transform = 'translateX(100%)';
-    play.querySelector('i.fas').classList.remove('fa-play');
-    play.querySelector('i.fas').classList.add('fa-pause');
-    music.play();
-    
+  controlPad.classList.add("play");
+  img.style.animationPlayState = "running";
+  display.style.transform = "translateY(-85%)";
+  play.querySelector("i.fas").classList.remove("fa-play");
+  play.querySelector("i.fas").classList.add("fa-pause");
+  music.play();
 }
 
 //pause music
 
 function musicPause() {
-    controlPad.classList.remove('play');
-    img.style.animationPlayState = 'paused';
-    display.style.transform = 'translateY(0)';
-    play.querySelector('i.fas').classList.remove('fa-pause');
-    play.querySelector('i.fas').classList.add('fa-play');
-    
-    music.pause();
+  controlPad.classList.remove("play");
+  img.style.animationPlayState = "paused";
+  display.style.transform = "translateY(0)";
+  play.querySelector("i.fas").classList.remove("fa-pause");
+  play.querySelector("i.fas").classList.add("fa-play");
+
+  music.pause();
 }
 
 //previous music
 
 function previousMusic() {
-    musicIndex --;
-    
-    if (musicIndex < 0) {
-        musicIndex = musicList.length - 1;
-    }
-    
-    loadMusic(musicList[musicIndex]);
-    
-    musicPlay();
-    
+  musicIndex--;
+
+  if (musicIndex < 0) {
+    musicIndex = musicList.length - 1;
+  }
+
+  loadMusic(musicList[musicIndex]);
+
+  musicPlay();
 }
 
 //next music
 
 function nextMusic() {
-    musicIndex ++;
+  musicIndex++;
 
-    if (musicIndex > musicList.length -1){
-        musicIndex = 0;
-    }
+  if (musicIndex > musicList.length - 1) {
+    musicIndex = 0;
+  }
 
-    loadMusic(musicList[musicIndex]);
-    musicPlay();
+  loadMusic(musicList[musicIndex]);
+  musicPlay();
 }
 
 //update progress
 
-
-function updateProgress (e) {
-    let { currentTime, duration } = e.target;
-    let progressPercent = (currentTime / duration) * 100;
-    progress.style.width = `${progressPercent}%`;
+function updateProgress(e) {
+  let { currentTime, duration } = e.target;
+  let progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
 
-
+//set progress
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = music.duration;
+  music.currentTime = (clickX / width) * duration;
+}
 
 //add event listener
 
-play.addEventListener('click', () => {
-    const isPlaying = controlPad.classList.contains('play');
+play.addEventListener("click", () => {
+  const isPlaying = controlPad.classList.contains("play");
 
-    if (isPlaying){
-        musicPause();
-    }else{
+  if (isPlaying) {
+    musicPause();
+  } else {
     musicPlay();
-    }
-    
+  }
 });
 
-previous.addEventListener('click', previousMusic);
-next.addEventListener('click', nextMusic);
+previous.addEventListener("click", previousMusic);
+next.addEventListener("click", nextMusic);
 
-music.addEventListener('timeupdate', updateProgress);
+music.addEventListener("timeupdate", updateProgress);
+
+progressContainer.addEventListener("click", setProgress);
+music.addEventListener("ended", nextMusic);
